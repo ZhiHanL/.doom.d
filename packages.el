b26bf7b4b9776e8c63145459a8309f3218edd523
Zhi Han <32226443+ZhiHanL@users.noreply.github.com>
2020-09-16 17:38:12 -0700

    Update packages.el

--- packages.el
+++ packages.el
@@ -49,9 +49,5 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
-(package! gdscript-mode
-          :recipe (:host github
-                   :repo "GDQuest/emacs-gdscript-mode"
-                   :files ("*.el" "data/*.txt")))
 (package! org-roam-server)
 (package! org-journal)
